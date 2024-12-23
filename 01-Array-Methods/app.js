93292ea36e9a71b505c5850950c36461663c1efc
Waseem Khalil <waseemkhalilworld@gmail.com>
2024-12-23 11:37:02 -0800

    understanding by array methods

---
+++ 01-Array-Methods/app.js
@@ -0,0 +1,54 @@
+// Array
+
+// const myArray=[1,2,3,4,5]
+// console.log(myArray[4]); //array find index starting point 0 index and others /5
+// console.log(myArray[10]); //array find index starting point 0 index and others /undefind
+
+//Array Methods
+
+const Array = [1, 2, 3, 4, 5];
+
+//Push method 1
+
+// Array.push(2)
+// Array.push(10,9,11,12) // push method are value pushing with array
+
+// Pop Method 2
+
+// Array.pop() // last array digit has been deleted this is called Pop
+
+//Shift Method 3
+
+// Array.shift() // Removing array of first Digit this called shift
+
+//Unshift Method 4
+
+// Array.unshift(12) // adding array in the first digit puting this called unshift
+
+//Includes Method 5
+
+// console.log(Array.includes(9));
+// The answer has been always bolean  true / false  includes meaning array mai include value agr ha to true ni ha to false
+
+//Indexof Method 6
+
+// console.log(Array.indexOf(1)) // Find Index of Array
+
+//Join Method 7
+
+// const newArr= Array.join() // Join method this is array numbers to convert string
+// console.log(newArr);
+// console.log(Array);
+
+// Slice Method 
+
+
+const myNewsArray =Array.slice(0,4)
+const myNewArray =Array.splice(0,4)
+
+console.log('A ' ,myNewsArray);
+console.log('B', myNewArray);
+
+// slice splice not understand the next day understood
+
+console.log(Array);
